86ac0c77a78499232509db3ef244e982d9bb15f1
Wayne Liu <>
2021-02-28 21:50:39 -0600

    add test

---
+++ leads/tests/test_view.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+
+# Create your tests here.
+class LandingPageTest(TestCase):
+    def test_get(self):
+        response = self.client.get(reverse("landing_page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
